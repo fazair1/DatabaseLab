7dc4a2d70b2ef45f9db74c9458651514dfc73a09
akhmadali1 <59761090+akhmadali1@users.noreply.github.com>
2022-01-02 23:33:23 +0700

    Update Ali_Data.sql

--- Ali_Data.sql
+++ Ali_Data.sql
@@ -2,3 +2,8 @@ INSERT INTO Staff VALUES
 ('ST001', 'Chicony Evy', 'chiconyevy081@hotmail.com', '084561820512', '73 Orange Point','2000-12-25','Female', 6500000),
 ('ST002', 'Red Adam', 'adamred102@ymail.com', '084758192385', '56 Wood Street','1985-01-11','Male', 11250000),
 ('ST003', 'Lilia Eru', 'Lieru900@coldmail.com', '084701824725', '200 Palm Vanda','1996-07-18','Female', 9725000);
+
+INSERT INTO Vendor VALUES 
+('VE001', 'Dindes Manafe', '088324018248', '42 Pandan Street', 'dinnafe092@gmail.com'),
+('VE002', 'Hallin Kaiser', '085293572934', '192 Manggo Lux', 'kaiserhall222@gmail.com'),
+('VE003', 'Rupton Zynga', '085912508249', '29 Aster Rum', 'ruptonzynga1@gmail.com');
