4e02940d81379623a50c9ffa6bdcc26899b8562b
akhmadali1 <59761090+akhmadali1@users.noreply.github.com>
2022-01-02 23:20:11 +0700

    Create Ali_Data.sql

---
+++ Ali_Data.sql
@@ -0,0 +1,4 @@
+INSERT INTO Staff VALUES 
+('ST001', 'Chicony Evy', 'chiconyevy081@hotmail.com', '084561820512', '73 Orange Point','2000-12-25','Female', 6500000),
+('ST002', 'Red Adam', 'adamred102@ymail.com', '084758192385', '56 Wood Street','1985-01-11','Male', 11250000),
+('ST003', 'Lilia Eru', 'Lieru900@coldmail.com', '084701824725', '200 Palm Vanda','1996-07-18','Female', 9725000);
