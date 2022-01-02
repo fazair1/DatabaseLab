c90a96e94705afa5c5de2748240b9e55eb545f53
fazair1 <fauzan.irfanto1@gmail.com>
2022-01-02 23:37:35 +0700

    Update InsertFauzan.sql

--- InsertFauzan.sql
+++ InsertFauzan.sql
@@ -1,8 +1,15 @@
 INSERT INTO Staff
-VALUES ('ST007','Ojan Irawan','ojanwan@gmail.com','086212349876','Jalan Rambutan RT 07 RW 01','2000-12-5','Male',1000000)
+VALUES 
+('ST007','Ojan Irawan','ojanwan@gmail.com','086212349876','Jalan Rambutan RT 07 RW 01','2000-12-5','Male',1000000),
+('ST008','Budi Jago','budija@gmail.com','086274562985','Jalan Mangga RT 01 RW 02','2000-1-9','Male',9000000),
+('ST009','Tama Wawa','tamawa@gmail.com','086227642456','Jalan Jeruk RT 02 RW 06','2000-4-6','Female',6000000);
 
-INSERT INTO Staff
-VALUES ('ST008','Budi Jago','budija@gmail.com','086274562985','Jalan Mangga RT 01 RW 02','2000-1-9','Male',9000000)
+INSERT INTO VENDOR
+VALUES 
+('VE007','Opi Opea','086254389462','Jalan Rujak RT 09 RW 07','opiea@gmail.com'),
+('VE008','Dimas Siu','086216782315','Jalan Otewe RT 03 RW 05','dimasu@gmail.com'),
+('VE009','Mirana Arrow','086218644863','Jalan Dire RT 03 RW 22','miranarow@gmail.com');
 
-INSERT INTO Staff
-VALUES ('ST009','Tama Wawa','tamawa@gmail.com','086227642456','Jalan Jeruk RT 02 RW 06','2000-4-6','Male',6000000)
+INSERT INTO Customer
+VALUES
+('CU007','
