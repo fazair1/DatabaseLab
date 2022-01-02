4e13dc3725fed082f6c0032692dba5d69412439f
DEONISAVAILABLE <96986774+DEONISAVAILABLE@users.noreply.github.com>
2022-01-02 23:22:30 +0700

    Add files via upload

---
+++ InsertStaffAxcel.sql
@@ -0,0 +1,13 @@
+
+INSERT INTO Staff VALUES 
+('ST004', 'Reinhart Tillman', 'TillHart@gmail.com', '085976390712', '21 Papandayan Fruit','1998-12-12','Male', 9500000)
+;
+
+INSERT INTO Staff VALUES 
+('ST005', 'Rina Roosevelt', 'choochoorina@gmail.com', '087757667992', '9 Baltimore Estate','1997-02-07','Female', 15000000)
+;
+
+INSERT INTO Staff VALUES 
+('ST006', 'Jenny Lindzey', 'JennyGurl@gmail.com', '081221345478', '11 Land Of Dawn','1999-06-17','Female', 8000000)
+;
+
