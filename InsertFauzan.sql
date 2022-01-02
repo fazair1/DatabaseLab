16d7bb104fc22dd7065c4f659ebd7989939b24c5
fazair1 <fauzan.irfanto1@gmail.com>
2022-01-02 23:24:29 +0700

    Create InsertFauzan.sql

---
+++ InsertFauzan.sql
@@ -0,0 +1,8 @@
+INSERT INTO Staff
+VALUES ('ST007','Ojan Irawan','ojanwan@gmail.com','086212349876','Jalan Rambutan RT 07 RW 01','2000-12-5','Male',1000000)
+
+INSERT INTO Staff
+VALUES ('ST008','Budi Jago','budija@gmail.com','086274562985','Jalan Mangga RT 01 RW 02','2000-1-9','Male',9000000)
+
+INSERT INTO Staff
+VALUES ('ST009','Tama Wawa','tamawa@gmail.com','086227642456','Jalan Jeruk RT 02 RW 06','2000-4-6','Male',6000000)
