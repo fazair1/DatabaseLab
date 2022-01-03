4c9601637c43f9d3012e5d2cfe1536073c9906f1
akhmadali1 <59761090+akhmadali1@users.noreply.github.com>
2022-01-03 23:06:51 +0700

    Update 3. Query to insert data into tables..sql

--- 3. Query to insert data into tables..sql
+++ 3. Query to insert data into tables..sql
@@ -9,7 +9,7 @@ INSERT INTO Staff VALUES
 ('ST006', 'Jenny Lindzey', 'JennyGurl@gmail.com', '081221345478', '11 Land Of Dawn','1999-06-17','Female', 8000000),
 ('ST007','Ojan Irawan','ojanwan@gmail.com','086212349876','Jalan Rambutan RT 07 RW 01','2000-12-5','Male',1000000),
 ('ST008','Budi Jago','budija@gmail.com','086274562985','Jalan Mangga RT 01 RW 02','2000-1-9','Male',9000000),
-('ST009','Tama Wawa','tamawa@gmail.com','086227642456','Jalan Jeruk RT 02 RW 06','2000-4-6','Female',6000000),
+('ST009','Tama Wawa','tamawa@gmail.com','086227642456','Jalan Jeruk RT 02 RW 06','2000-4-6','Female',6000000);
 
 INSERT INTO Vendor VALUES 
 ('VE001', 'Dindes Manafe', '088324018248', '42 Pandan Street', 'dinnafe092@gmail.com'),
@@ -20,7 +20,7 @@ INSERT INTO Vendor VALUES
 ('VE006', 'Gusion Paxley', '082358975648', '5 Aberleen Cluster', 'emperorgusion@gmail.com'),
 ('VE007','Opi Opea','086254389462','Jalan Rujak RT 09 RW 07','opiea@gmail.com'),
 ('VE008','Dimas Siu','086216782315','Jalan Otewe RT 03 RW 05','dimasu@gmail.com'),
-('VE009','Mirana Arrow','086218644863','Jalan Dire RT 03 RW 22','miranarow@gmail.com'),
+('VE009','Mirana Arrow','086218644863','Jalan Dire RT 03 RW 22','miranarow@gmail.com');
 
 INSERT INTO Customer VALUES 
 ('CU001', 'Boxin Pacloss', '085278592105', '304 Citarum Street', 'Male', 'boxloss@fmail.com','2002-05-17'),
@@ -31,7 +31,7 @@ INSERT INTO Customer VALUES
 ('CU006', 'Sunarto Up', '088876554765', '46 Antapani Reprepan', 'Male', 'baratsjoget@gmail.com','1958-12-29'),
 ('CU007','Sapei Derman','086274289532','Jalan Ben RT 10 RW 03','Male','speiderman@gmail.com','2004-8-25'),
 ('CU008','Hulek Ijo','086268425734','Jalan Smash RT 08 RW 09','Male','hulekijo@gmail.com','2006-10-24'),
-('CU009','Thoriq Thunder','086218760953','Jalan Val RT 01 RW 10','Male','thoriqthunder@gmail.com','2010-7-10'),
+('CU009','Thoriq Thunder','086218760953','Jalan Val RT 01 RW 10','Male','thoriqthunder@gmail.com','2010-7-10');
 
 INSERT INTO BionicType VALUES 
 ('TY001', 'Eye', 65),
@@ -42,7 +42,7 @@ INSERT INTO BionicType VALUES
 ('TY006', 'Eye', 71),
 ('TY007','Foot',78),
 ('TY008','Leg',80),
-('TY009','Eye',100),
+('TY009','Eye',100);
 
 INSERT INTO Bionic VALUES 
 ('BI001', 'TY002', 'Riot Wrest', 3 ,'2000-12-25', 925000),
@@ -53,4 +53,40 @@ INSERT INTO Bionic VALUES
 ('BI006', 'TY006', 'Ignite', 13 ,'2015-07-23', 925000),
 ('BI007','TY009','Sharingan',15,'2020-12-30',5000000),
 ('BI008','TY007','AX007',10,'2015-10-24',3500000),
-('BI009','TY008','Terminator',13,'2021-6-17',4250000),
+('BI009','TY008','Terminator',13,'2021-6-17',4250000);
+
+INSERT INTO PurchaseTransaction VALUES 
+('PU001', 'ST004', 'VE002','2019-01-19'),
+('PU002', 'ST002', 'VE004','2019-05-04'),
+('PU003', 'ST003', 'VE001','2019-06-12'),
+('PU004', 'ST001', 'VE002','2019-08-10'),
+('PU005', 'ST004', 'VE003','2019-11-25'),
+('PU006', 'ST002', 'VE004','2019-12-30'),
+('PU007', 'ST001', 'VE003','2019-12-01');
+
+INSERT INTO SalesTransaction VALUES 
+('SA001', 'ST002', 'CU004','2019-06-02'),
+('SA002', 'ST004', 'CU002','2019-08-14'),
+('SA003', 'ST001', 'CU003','2019-09-23'),
+('SA004', 'ST002', 'CU001','2019-10-25'),
+('SA005', 'ST004', 'CU002','2019-11-01'),
+('SA006', 'ST002', 'CU004','2019-11-02'),
+('SA007', 'ST003', 'CU001','2019-12-30');
+
+INSERT INTO PurchaseTransactionDetail VALUES 
+('PU001', 'BI001', 5),
+('PU002', 'BI001', 8),
+('PU003', 'BI003', 10),
+('PU004', 'BI002', 1),
+('PU005', 'BI001', 11),
+('PU006', 'BI003', 20),
+('PU007', 'BI003', 2);
+
+INSERT INTO SalesTransactionDetail VALUES 
+('SA001', 'BI003', 6),
+('SA002', 'BI001', 3),
+('SA003', 'BI003', 18),
+('SA004', 'BI002', 1),
+('SA005', 'BI001', 10),
+('SA006', 'BI003', 5),
+('SA007', 'BI001', 6);
