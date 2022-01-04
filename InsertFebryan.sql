4cacca1cf00c622050c61b2390b63f8e4ca89def
SantaiKalemEnjoy <80592979+SantaiKalemEnjoy@users.noreply.github.com>
2022-01-04 16:00:07 +0700

    Add files via upload

---
+++ InsertFebryan.sql
@@ -0,0 +1,30 @@
+INSERT INTO Staff 
+VALUES 
+('ST010', 'Edy Sutedja', 'edsu@hotmail.com', '087854963024', 'Jalan Jeruk RT 4 RW 17','1998-5-24','Male', 8500000),
+('ST011', 'Bambang Sutanto', 'bamsut@yahoo.com', '081548572065', 'Jalan Sabar RT 10 RW 19','1977-11-17','Male', 15000000),
+('ST012', 'Rachel Anastasia', 'anaspanas@coldmail.com', '082390152218', 'Serpong Terrace D17','1985-03-31','Female', 7500000);
+
+INSERT INTO Vendor 
+VALUES 
+('VE010', 'Alifthio Suherman', '0877795438055', 'BSD Park unit 404', 'Alifganteng2@gmail.com'),
+('VE011', 'James Volt', '085677952423', 'Bintaro Residence F23', 'nyetrum@gmail.com'),
+('VE012', 'Alfred Pajero', '085720145254', 'Jalan Damai RT 7 RW 7', 'pajero1@gmail.com');
+
+INSERT INTO Customer 
+VALUES 
+('CU010', 'Andrew Persia', '088858689828', 'Jalan Cendrawasih 1 RT 9 RW 19', 'Male', 'Andrew2013@rockmail.com','2002-08-27'),
+('CU011', 'Abigail Gabrina', '089924658125', 'Jalan Ceger kavling 2458', 'Female', 'abicaantik@sumail.com','2002-10-12'),
+('CU012', 'Sarah Citra', '084957154685', 'Kebayoran Terrace C3', 'Female', 'sarter@atoyot.com','1999-01-4');
+
+INSERT INTO BionicType 
+VALUES 
+('TY010', 'Arm', 72),
+('TY011', 'Leg', 78),
+('TY012', 'Eye', 97);
+
+INSERT INTO Bionic 
+VALUES 
+('BI010', 'TY011', 'Zenirex Bar', 4 ,'2003-10-7', 785000),
+('BI011', 'TY010', 'Fuji Tela', 2 ,'1989-04-22', 4285000),
+('BI012', 'TY012', 'Orico Sang', 9 ,'2001-09-09', 125000);
+
