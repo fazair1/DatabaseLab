b5daaa74353402dfb9e5807a202c736621540189
fazair1 <fauzan.irfanto1@gmail.com>
2022-01-04 16:48:49 +0700

    progress Insert Fauzan

--- 3. Query to insert data into tables..sql
+++ 3. Query to insert data into tables..sql
@@ -7,9 +7,13 @@ INSERT INTO Staff VALUES
 ('ST004', 'Reinhart Kjelberg', 'TillHart@gmail.com', '085976390712', '21 Papandayan Fruit','1998-12-12','Male', 9500000),
 ('ST005', 'Rina Roosevelt', 'choochoorina@gmail.com', '087757667992', '9 Baltimore Estate','1997-02-07','Female', 15000000),
 ('ST006', 'Jenny Lindzey', 'JennyGurl@gmail.com', '081221345478', '11 Land Of Dawn','1999-06-17','Female', 8000000),
-('ST007','Ojan Irawan','ojanwan@gmail.com','086212349876','Jalan Rambutan RT 07 RW 01','2000-12-5','Male',1000000),
-('ST008','Budi Jago','budija@gmail.com','086274562985','Jalan Mangga RT 01 RW 02','2000-1-9','Male',9000000),
-('ST009','Tama Wawa','tamawa@gmail.com','086227642456','Jalan Jeruk RT 02 RW 06','2000-4-6','Female',6000000);
+('ST007', 'Ojan Irawan','ojanwan@gmail.com','086212349876','Jalan Rambutan RT 07 RW 01','2000-12-5','Male',1000000),
+('ST008', 'Budi Jago','budija@gmail.com','086274562985','Jalan Mangga RT 01 RW 02','2000-1-9','Male',9000000),
+('ST009', 'Tama Wawa','tamawa@gmail.com','086227642456','Jalan Jeruk RT 02 RW 06','2000-4-6','Female',6000000),
+('ST010', 'Edy Sutedja', 'edsu@hotmail.com', '087854963024', 'Jalan Jeruk RT 4 RW 17','1998-5-24','Male', 8500000),
+('ST011', 'Bambang Sutanto', 'bamsut@yahoo.com', '081548572065', 'Jalan Sabar RT 10 RW 19','1977-11-17','Male', 15000000),
+('ST012', 'Rachel Anastasia', 'anaspanas@coldmail.com', '082390152218', 'Serpong Terrace D17','1985-03-31','Female', 7500000);
+
 
 INSERT INTO Vendor VALUES 
 ('VE001', 'Dindes Manafe', '088324018248', '42 Pandan Street', 'dinnafe092@gmail.com'),
@@ -18,9 +22,13 @@ INSERT INTO Vendor VALUES
 ('VE004', 'Randy Tillman', '081299878413', '52 Ares Street', 'randyman@gmail.com'),
 ('VE005', 'Guinevere Baroque', '087785976844', '37 Castle George', 'supermagicguin@gmail.com'),
 ('VE006', 'Gusion Paxley', '082358975648', '5 Aberleen Cluster', 'emperorgusion@gmail.com'),
-('VE007','Opi Opea','086254389462','Jalan Rujak RT 09 RW 07','opiea@gmail.com'),
-('VE008','Dimas Siu','086216782315','Jalan Otewe RT 03 RW 05','dimasu@gmail.com'),
-('VE009','Mirana Arrow','086218644863','Jalan Dire RT 03 RW 22','miranarow@gmail.com');
+('VE007', 'Opi Opea','086254389462','Jalan Rujak RT 09 RW 07','opiea@gmail.com'),
+('VE008', 'Dimas Siu','086216782315','Jalan Otewe RT 03 RW 05','dimasu@gmail.com'),
+('VE009', 'Mirana Arrow','086218644863','Jalan Dire RT 03 RW 22','miranarow@gmail.com'),
+('VE010', 'Alifthio Suherman', '0877795438055', 'BSD Park unit 404', 'Alifganteng2@gmail.com'),
+('VE011', 'James Volt', '085677952423', 'Bintaro Residence F23', 'nyetrum@gmail.com'),
+('VE012', 'Alfred Pajero', '085720145254', 'Jalan Damai RT 7 RW 7', 'pajero1@gmail.com');
+
 
 INSERT INTO Customer VALUES 
 ('CU001', 'Boxin Pacloss', '085278592105', '304 Citarum Street', 'Male', 'boxloss@fmail.com','2002-05-17'),
@@ -29,9 +37,13 @@ INSERT INTO Customer VALUES
 ('CU004', 'Ramado Dragomax', '083254665432', '20 simpang leomord', 'Male', 'salamdaribinjai@gmail.com','2001-11-21'),
 ('CU005', 'Hakku Venna', '085567432917', 'D9 The Suites', 'Female', 'bananamcd@gmail.com','2003-05-01'),
 ('CU006', 'Sunarto Up', '088876554765', '46 Antapani Reprepan', 'Male', 'baratsjoget@gmail.com','1958-12-29'),
-('CU007','Sapei Derman','086274289532','Jalan Ben RT 10 RW 03','Male','speiderman@gmail.com','2004-8-25'),
-('CU008','Hulek Ijo','086268425734','Jalan Smash RT 08 RW 09','Male','hulekijo@gmail.com','2006-10-24'),
-('CU009','Thoriq Thunder','086218760953','Jalan Val RT 01 RW 10','Male','thoriqthunder@gmail.com','2010-7-10');
+('CU007', 'Sapei Derman','086274289532','Jalan Ben RT 10 RW 03','Male','speiderman@gmail.com','2004-8-25'),
+('CU008', 'Hulek Ijo','086268425734','Jalan Smash RT 08 RW 09','Male','hulekijo@gmail.com','2006-10-24'),
+('CU009', 'Thoriq Thunder','086218760953','Jalan Val RT 01 RW 10','Male','thoriqthunder@gmail.com','2010-7-10'),
+('CU010', 'Andrew Persia', '088858689828', 'Jalan Cendrawasih 1 RT 9 RW 19', 'Male', 'Andrew2013@rockmail.com','2002-08-27'),
+('CU011', 'Abigail Gabrina', '089924658125', 'Jalan Ceger kavling 2458', 'Female', 'abicaantik@sumail.com','2002-10-12'),
+('CU012', 'Sarah Citra', '084957154685', 'Kebayoran Terrace C3', 'Female', 'sarter@atoyot.com','1999-01-4');
+
 
 INSERT INTO BionicType VALUES 
 ('TY001', 'Eye', 65),
@@ -40,9 +52,13 @@ INSERT INTO BionicType VALUES
 ('TY004', 'Hand', 100),
 ('TY005', 'Leg', 88),
 ('TY006', 'Eye', 71),
-('TY007','Foot',78),
-('TY008','Leg',80),
-('TY009','Eye',100);
+('TY007', 'Foot', 78),
+('TY008', 'Leg', 80),
+('TY009', 'Eye', 100),
+('TY010', 'Arm', 72),
+('TY011', 'Leg', 78),
+('TY012', 'Eye', 97);
+
 
 INSERT INTO Bionic VALUES 
 ('BI001', 'TY002', 'Riot Wrest', 3 ,'2000-12-25', 925000),
@@ -51,9 +67,15 @@ INSERT INTO Bionic VALUES
 ('BI004', 'TY004', 'Encode', 2 ,'2021-11-04', 4500000),
 ('BI005', 'TY005', 'SeaBasilisk', 5 ,'2009-01-27', 2800000),
 ('BI006', 'TY006', 'Ignite', 13 ,'2015-07-23', 925000),
-('BI007','TY009','Sharingan',15,'2020-12-30',5000000),
-('BI008','TY007','AX007',10,'2015-10-24',3500000),
-('BI009','TY008','Terminator',13,'2021-6-17',4250000);
+('BI007', 'TY009', 'Sharingan',15,'2020-12-30',5000000),
+('BI008', 'TY007', 'AX007',10,'2015-10-24',3500000),
+('BI009', 'TY008', 'Terminator',13,'2021-6-17',4250000),
+('BI010', 'TY011', 'Zenirex Bar', 4 ,'2003-10-7', 785000),
+('BI011', 'TY010', 'Fuji Tela', 2 ,'1989-04-22', 4285000),
+('BI012', 'TY012', 'Orico Sang', 9 ,'2001-09-09', 125000);
+
+
+--Transaction Table
 
 INSERT INTO PurchaseTransaction VALUES 
 ('PU001', 'ST004', 'VE002','2019-01-19'),
@@ -69,7 +91,15 @@ INSERT INTO PurchaseTransaction VALUES
 ('PU011', 'ST005', 'VE006','2020-02-18'),
 ('PU012', 'ST008', 'VE007','2020-02-18'),
 ('PU013', 'ST006', 'VE008','2020-05-19'),
-('PU014', 'ST005', 'VE007','2020-06-29');
+('PU014', 'ST005', 'VE007','2020-06-29'),
+('PU015', 'ST009', 'VE007','2018-04-08'),
+('PU016', 'ST007', 'VE008','2018-07-12'),
+('PU017', 'ST008', 'VE009','2018-10-09'),
+('PU018', 'ST007', 'VE007','2018-07-25'),
+('PU019', 'ST009', 'VE008','2018-03-01'),
+('PU020', 'ST008', 'VE009','2018-08-08'),
+('PU021', 'ST007', 'VE009','2018-11-05'),
+
 
 INSERT INTO SalesTransaction VALUES 
 ('SA001', 'ST002', 'CU004','2019-06-02'),
@@ -85,7 +115,17 @@ INSERT INTO SalesTransaction VALUES
 ('SA011', 'ST006', 'CU005','2020-02-23'),
 ('SA012', 'ST008', 'CU006','2020-03-07'),
 ('SA013', 'ST006', 'CU008','2020-04-12'),
-('SA014', 'ST007', 'CU005','2020-05-31');
+('SA014', 'ST007', 'CU005','2020-05-31'),
+('SA015', 'ST007', 'CU007','2020-09-03'),
+('SA016', 'ST007', 'CU008','2020-06-12'),
+('SA017', 'ST007', 'CU009','2020-11-16'),
+('SA018', 'ST008', 'CU007','2020-08-22'),
+('SA019', 'ST008', 'CU009','2020-07-07'),
+('SA020', 'ST009', 'CU008','2020-01-27'),
+('SA021', 'ST009', 'CU009','2020-02-09'),
+
+
+--Transaction Detail Table
 
 INSERT INTO PurchaseTransactionDetail VALUES 
 ('PU001', 'BI001', 5),
@@ -101,7 +141,15 @@ INSERT INTO PurchaseTransactionDetail VALUES
 ('PU011', 'BI004', 2),
 ('PU012', 'BI005', 12),
 ('PU013', 'BI006', 30),
-('PU014', 'BI004', 3);
+('PU014', 'BI004', 3),
+('PU015', 'BI007', 7),
+('PU016', 'BI008', 3),
+('PU017', 'BI009', 7),
+('PU018', 'BI007', 5),
+('PU019', 'BI008', 7),
+('PU020', 'BI009', 6),
+('PU021', 'BI007', 3),
+ 
 
 INSERT INTO SalesTransactionDetail VALUES 
 ('SA001', 'BI003', 6),
@@ -117,4 +165,11 @@ INSERT INTO SalesTransactionDetail VALUES
 ('SA011', 'BI004', 2),
 ('SA012', 'BI006', 20),
 ('SA013', 'BI005', 7),
-('SA014', 'BI006', 9);
+('SA014', 'BI006', 9),
+('SA015', 'BI007', 1),
+('SA016', 'BI008', 2),
+('SA017', 'BI009', 4),
+('SA018', 'BI007', 8),
+('SA019', 'BI008', 5),
+('SA020', 'BI009', 3),
+('SA021', 'BI008', 1),
