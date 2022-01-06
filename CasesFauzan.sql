54072d5c6956681057a5e4b81ace4b5f34eecb66
fazair1 <fauzan.irfanto1@gmail.com>
2022-01-07 00:23:22 +0700

    Case 3 fix

--- CasesFauzan.sql
+++ CasesFauzan.sql
@@ -1,5 +1,8 @@
 --3
-SELECT st.SalesID, CustomerName, CustomerGender, [Total Quantity Purchased] = SUM(SalesQuantity)
+SELECT st.SalesID, CustomerName, CustomerGender, [Total Quantity Purchased] = SUM(SalesQuantity), [Total Bionic Purchased]= COUNT(BionicID), [Sales Date] = (CONVERT(varchar,SalesDate,7))
 	FROM SalesTransaction st 
 			JOIN SalesTransactionDetail std ON st.SalesID=std.SalesID
 			JOIN Customer c ON st.CustomerID=c.CustomerID
+	WHERE CustomerGender LIKE 'Female'
+	GROUP BY st.SalesID, CustomerName, CustomerGender, SalesDate
+	HAVING SUM(SalesQuantity) > 7
